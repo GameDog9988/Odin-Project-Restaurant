989161591ded6b5c3d6099d55c5d0ca0eea0346c
GameDog9988 <51346343+GameDog9988@users.noreply.github.com>
2020-07-10 14:27:32 -0400

    Fixed menu page and work started on contact page

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,3 +1,20 @@
 const contactPage = () => {
   const tabContent = document.querySelector("#tabContent");
+
+  const contact = [
+    "Cookie Pie",
+    "2848 Connecticut Avenue",
+    "Pittsburgh, PA 15216",
+    "412-576-2807",
+    "alex@grattan.me",
+  ];
+
+  contact.forEach(
+    (line) =>
+      (_createElement(parent, "p", {
+        class: "contact-info",
+      }).textContent = line)
+  );
 };
+
+export { contactPage };
--- src/menuPage.js
+++ src/menuPage.js
@@ -1,38 +1,23 @@
 const menuPage = () => {
   const tabContent = document.querySelector("#tabContent");
 
-  const cookies = [
-    {
-      name: "Chocolate Chip Cookie",
-      price: "$3",
-      description: "The ultimate classic for classic folk",
-    },
-    {
-      name: "Supreme Cookie",
-      price: "$5",
-      description: "It got everything baby",
-    },
-    {
-      name: "Snickerdoodle",
-      price: "3",
-      description: "A modern take on your Grandma's favorite",
-    },
-    {
-      name: "Milk",
-      price: "$2",
-      description: "Just milk",
-    },
-    {
-      name: "Almondmilk",
-      price: "$2",
-      description: "This the stuff my parents drink",
-    },
-    {
-      name: "Iced Coffee",
-      price: "$4",
-      description: "Gives a little pep in your step",
-    },
-  ];
+  const cookies = [];
+
+  const Cookie = (name, price) => {
+    const getName = () => console.log(name);
+    const getPrice = () => console.log(price);
+
+    return { name, price, getName, getPrice };
+  };
+
+  function addCookieToMenu(name, price) {
+    cookies.push(Cookie(name, price));
+  }
+
+  addCookieToMenu("Chocolate Chip Cookie", "$2");
+  addCookieToMenu("Kitchen Sink Cookie", "$4");
+  addCookieToMenu("Snickerdoodle Cookie", "$2");
+  addCookieToMenu("Peanut Butter Thumbdrop", "$3");
 
   const createMenuContainer = () => {
     const menuTitle = document.createElement("h1");
@@ -46,54 +31,16 @@ const menuPage = () => {
     tabContent.appendChild(menuContainer);
 
     const createMenuItems = () => {
-      for (let i = 0; i < cookies.length; i++) {
+      cookies.forEach((item) => {
         const menuCard = document.createElement("div");
-
+        menuCard.innerHTML = `${item.name}  <br>  ${item.price}`;
         menuCard.classList.add("menu-card");
-        menuCard.setAttribute(
-          "data-order",
-          `${menuContainer.childNodes.length}`
-        );
-        tabContent.appendChild(menuCard);
-      }
+        menuContainer.appendChild(menuCard);
+      });
     };
     createMenuItems();
   };
   createMenuContainer();
-
-  const menuItems = document.querySelectorAll(".menu-card");
-  menuItems.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-      item.classList.add("card-hover");
-
-      const text1 = document.createElement("div");
-      text1.textContent = cookies[item.dataset.order].name;
-      text1.classList.add("card-info");
-      text1.style.cssText =
-        "top: 15%; left: 50%; transform: translate(-50%, -50%); border-bottom: 1px solid";
-      item.appendChild(text1);
-
-      const text2 = document.createElement("div");
-      text2.textContent = cookies[item.dataset.order].price;
-      text2.classList.add("card-info");
-      text2.style.cssText =
-        "top: 45%; left: 50%; transform: translate(-50%, -50%);";
-      item.appendChild(text2);
-
-      const text3 = document.createElement("div");
-      text3.textContent = cookies[item.dataset.order].description;
-      text3.classList.add("card-info");
-      text3.style.cssText =
-        "top: 75%; left: 50%; transform: translate(-50%, -50%);";
-      item.appendChild(text3);
-    });
-    item.addEventListener("mouseleave", () => {
-      item.classList.remove("card-hover");
-      while (item.childNodes.length > 1) {
-        item.removeChild(item.lastChild);
-      }
-    });
-  });
 };
 
 export { menuPage };
